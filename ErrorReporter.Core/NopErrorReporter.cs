f340e7f0ab9bba12ef74d9f79dce6bb3e0023b3e
Steven Dawkins <sdawkins@oxfordeconomics.com>
2016-03-15 16:39:02 +0000

    Added NopErrorReporter

---
+++ ErrorReporter.Core/NopErrorReporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorReporter.Core
+{
+    public class NopErrorReporter : IErrorReporter
+    {
+        public void Capture(Exception e)
+        {
+        }
+
+        public void Capture(string message, Level level)
+        {
+        }
+    }
+}
