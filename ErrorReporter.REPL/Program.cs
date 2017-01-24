1ff3bcada391c1f2ad063679b80b212cb4492917
Steven Dawkins <sdawkins@oxfordeconomics.com>
2017-01-24 11:00:24 +0000

    added clearscript repl

---
+++ ErrorReporter.REPL/Program.cs
@@ -0,0 +1,19 @@
+﻿using Replify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorReporter.REPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repl = new ClearScriptRepl();
+
+            repl.StartReplLoop(args);
+        }
+    }
+}
---
+++ ErrorReporter.REPL/SentryCommand.cs
@@ -0,0 +1,23 @@
+﻿using Replify;
+using ErrorReporter.Sentry;
+using ErrorReporter.Core;
+
+namespace ErrorReporter.REPL
+{
+    public class SentryCommand : IReplCommand
+    {
+        private readonly SentryErrorReporter sentry;
+
+        public SentryCommand()
+        {
+            var url = "https://8b222a8aaba8417db60255cbcdc6381b:91af66fba0f94655a930e8b9acade6e2@sentry.io/108735";
+
+            this.sentry = new SentryErrorReporter(url);
+        }
+
+        public void Info(string message)
+        {
+            this.sentry.Capture(message, Level.Info);
+        }
+    }
+}
