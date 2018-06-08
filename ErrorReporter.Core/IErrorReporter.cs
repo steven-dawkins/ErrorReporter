5f98ec920321dd7d92d886a507b66b3800a0d0b6
Steven Dawkins <sdawkins@oxfordeconomics.com>
2018-06-08 10:06:18 +0100

    added debug log level

--- ErrorReporter.Core/IErrorReporter.cs
+++ ErrorReporter.Core/IErrorReporter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ErrorReporter.Core
 {
-    public enum Level { Error, Info }
+    public enum Level { Error, Info, Debug }
 
     public interface IErrorReporter
     {
