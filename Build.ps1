db0941e861a03c28316aa6995f921d9c1ba07953
Steven Dawkins <sdawkins@oxfordeconomics.com>
2018-12-27 22:37:40 +0000

    build tweaks

--- Build.ps1
+++ Build.ps1
@@ -2,7 +2,7 @@
 
 dotnet restore
 
-# Invoke-MSBuild
+dotnet build -c Release
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 # $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
